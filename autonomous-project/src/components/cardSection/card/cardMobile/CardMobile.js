583594912a80603f09af838804c67df35955da1d
Volodymyr Benzyk <redrollofficial@gmail.com>
2023-11-02 13:16:18 +0100

    Bug fixes in slider

--- autonomous-project/src/components/cardSection/card/cardMobile/CardMobile.js
+++ autonomous-project/src/components/cardSection/card/cardMobile/CardMobile.js
@@ -10,22 +10,54 @@ const CardMobile = props => {
 
     const [dynamicStyle, setDynamicStyle] = useState('next-slide')
     const [slideIndex, setSlideIndex] = useState(0)
+    const [clicked, setCliked] = useState({
+        prev: '',
+        next: ''
+        
+    })
+
+   
 
     const nextClickHandler = () => {
         setSlideIndex(prevState => prevState === props.data.length - 1 ? 0 : prevState + 1)
         setDynamicStyle(() => 'next-slide')
+        setCliked(() => {
+            return {
+                next: 'active-button'
+            }
+        })
+        setTimeout(() => {
+            setCliked(() => {
+                return {
+                    next: ''
+                }
+            })
+        }, 300);
     }
 
     const prevClickHandler = () => {
         setSlideIndex(prevState => prevState === 0 ? props.data.length - 1 : prevState - 1) // якщо замість props.data.length - 1 написати просто props.data.length, то на останньому слайді виб'є помилку (бо slideIndex буде 4, а елементу з таким порядковим номером немає в массиві даних)
         setDynamicStyle(() => 'prev-slide')
+        setCliked(() => {
+            return {
+                prev: 'active-button'
+            }
+        })
+        setTimeout(() => {
+            setCliked(() => {
+                return {
+                    prev: ' '
+                }
+            })
+        }, 300);
     }
 
     const childForsePushClass = dynamicClass => child => cloneElement(child, {
         classNames: dynamicClass
     })
 
-    console.log(dynamicStyle)
+    const classes1 = `mobile-button0 ${clicked.prev}`
+    const classes2 = `mobile-button1 ${clicked.next}`
 
     return (
 
@@ -33,10 +65,9 @@ const CardMobile = props => {
 
 
             <div className='slide-wrapper'>
-                <div className='mobile-buttons'>
-                    <Button className='mobile-button0' onClick={prevClickHandler}>{'<'}</Button>
-                    <Button className='mobile-button1' onClick={nextClickHandler}>{'>'}</Button>
-                </div>
+                
+                <Button className={classes1} onClick={prevClickHandler}>{'<'}</Button>
+
                 <TransitionGroup childFactory={childForsePushClass(dynamicStyle)}>
                     <CSSTransition key={props.data[slideIndex].img} timeout={1000} classNames={dynamicStyle}>
 
@@ -47,6 +78,7 @@ const CardMobile = props => {
                     </CSSTransition>
                 </TransitionGroup>
 
+                <Button className={classes2} onClick={nextClickHandler}>{'>'}</Button>
             </div>
 
         </div>
